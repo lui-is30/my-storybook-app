463f610fdc0244871b8d8ca82207639383af3e8d
luis.huaman <luisitohblas@gmail.com>
2025-04-14 15:25:09 +0200

    with intercations

--- src/Button.stories.ts
+++ src/Button.stories.ts
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Button } from "./Button";
+import { within } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 const meta: Meta<typeof Button> = {
     component: Button,
@@ -16,3 +18,24 @@ export const Primary: Story = {
         onClick: () => alert('Clicked!'),
     },
 };
+
+/**
+ * nuova storia per il bottone
+ * modificare il componente Button per supportare la disabilitazione
+ * Aggiungere una proprietà per disabilitarlo
+ * Se la proprietà è null gestire all'onclick prima la disabilitazione
+ * Simulare l'operazione del click con un setTimeOut di 5 secondi
+ */
+
+export const Second: Story = {
+    args: {
+      label: "Click Me",
+      onClick: () => alert("Operazione completata!"),
+      isDisabled: null,
+    },
+    play:async({canvasElement})=>{
+        const canvas=within(canvasElement);
+        const button=canvas.getByRole("button");
+        await userEvent.click(button);
+    },
+  };
--- src/Button.tsx
+++ src/Button.tsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 type ButtonProps = {
     label: string;
     onClick: () => void;
+    isDisabled:boolean|null;
 };
 
-export const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
-    return <button onClick={onClick}>{label}</button>
+export const Button: React.FC<ButtonProps> = ({ label, onClick,isDisabled=false }) => {
+
+        
+    const [localDisabled, setLocalDisabled] = useState(false);
+
+    const handleClick = () => {
+     
+      if (isDisabled === null) {
+        setLocalDisabled(true);
+  
+       
+        setTimeout(() => {
+          onClick();
+          setLocalDisabled(false);
+        }, 5000);
+      } else {
+       
+        onClick();
+      }
+    };
+  
+    return (
+      <button onClick={handleClick} disabled={isDisabled === true || localDisabled}>
+        {label}
+      </button>
+    );
 };
